534eefb2fa0b6974844e923d8a7909f47d7db61d
SlavisaTasic <slavisatasic82@gmail.com>
2017-02-22 20:17:06 +0400

    test

--- try.sh
+++ try.sh
@@ -1,1 +1,10 @@
-cat 01_VTBTRE.xls | grep -i -e '</\?TABLE\|</\?TD\|</\?TR\|</\?TH' | sed 's/^[\ \t]*//g' | tr -d '\n' | sed 's/<\/TR[^>]*>/\n/Ig'  | sed 's/<\/\?\(TABLE\|TR\)[^>]*>//Ig' | sed 's/^<T[DH][^>]*>\|<\/\?T[DH][^>]*>$//Ig' | sed 's/<\/T[DH][^>]*><T[DH][^>]*>/,/Ig'
+#!/bin/bash
+
+cat $1 | \
+ grep -i -e '</\?TABLE\|</\?TD\|</\?TR\|</\?TH' | \
+ sed 's/^[\ \t]*//g' | \
+ tr -d '\n' | \
+ sed 's/<\/TR[^>]*>/\n/Ig'  | \
+ sed 's/<\/\?\(TABLE\|TR\)[^>]*>//Ig' | \
+ sed 's/^<T[DH][^>]*>\|<\/\?T[DH][^>]*>$//Ig' | \
+ sed 's/<\/T[DH][^>]*><T[DH][^>]*>/,/Ig'
