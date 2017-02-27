31404ebac01b8939852d2f30947f5c3112dbde2d
SlavisaTasic <slavisatasic82@gmail.com>
2017-02-27 14:43:11 +0400

    reverted old WhatConvert

--- WhatConvert/ConvertRaiffeisen.sh
+++ WhatConvert/ConvertRaiffeisen.sh
@@ -3,15 +3,15 @@
 
 CurrentDate='date +%d.%m.%Y'
 
-# откатить!!!
-
-
 for i in ./Prices/`$CurrentDate`/r/*.xls;
 do
-	Path=$(dirname $i);
-	FullName=$(basename $i);
-	NameWithoutExt=$(echo "${FullName%%.*}");
-	ssconvert $i $NameWithoutExt".csv";
-	awk -f ./WhatConvert/command.awk $NameWithoutExt".csv" > $NameWithoutExt".csv2";
-	mv $NameWithoutExt".csv2" $NameWithoutExt".csv";
+ 	Path=$(dirname $i);
+ 	FullName=$(basename $i);
+ 	NameWithoutExt=$(echo "${FullName%%.*}");
+ 	grep -i -e '<content>' "$i" > $NameWithoutExt".csv";
+ 	sed -i 's/^.*<content>//g' $NameWithoutExt".csv";
+ 	sed -i 's/<\/content>//g' $NameWithoutExt".csv";
+#	sed 's/(?<=\d)/\n/g' "$i" > $NameWithoutExt".csv";
+ 	awk -f ./WhatConvert/command.awk $NameWithoutExt".csv" > $NameWithoutExt".csv2";
+ 	mv $NameWithoutExt".csv2" $NameWithoutExt".csv";
 done
