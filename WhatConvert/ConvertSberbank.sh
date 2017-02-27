dd5a7f9f585d6c96a0d3e4a725e7f9b5b4e172c0
SlavisaTasic <slavisatasic82@gmail.com>
2017-02-27 14:51:22 +0400

    tested ConvertSberbank

--- WhatConvert/ConvertSberbank.sh
+++ WhatConvert/ConvertSberbank.sh
@@ -8,11 +8,13 @@ do
  	Path=$(dirname $i);
  	FullName=$(basename $i);
  	NameWithoutExt=$(echo "${FullName%%.*}");
- 	sed '/^.*<table border="1"><tr><td>/!d' "$i" > $NameWithoutExt".csv"; # all strings before last
- 	sed -i 's/^.*<table border="1"><tr><td>//g' $NameWithoutExt".csv"; # insert ,
- 	sed -i 's/<\/td><td>/,/g' $NameWithoutExt".csv"; # insert ,
- 	sed -i 's/<\/td><\/tr><tr><td align="left" valign="middle">/\
-/g' $NameWithoutExt".csv"; # insert \n
- 	sed -i 's/<\/td><td align="right" valign="middle" style="mso-number-format:0\\.00">/,/g' $NameWithoutExt".csv"; # insert ,
- 	sed -i 's/<\/td><\/tr><\/table><\/body><\/html>//g' $NameWithoutExt".csv"; # all text in line after table
+ 	echo $Path
+ 	echo $NameWithoutExt
+#  	sed '/^.*<table border="1"><tr><td>/!d' "$i" > $NameWithoutExt".csv"; # all strings before last
+#  	sed -i 's/^.*<table border="1"><tr><td>//g' $NameWithoutExt".csv"; # insert ,
+#  	sed -i 's/<\/td><td>/,/g' $NameWithoutExt".csv"; # insert ,
+#  	sed -i 's/<\/td><\/tr><tr><td align="left" valign="middle">/\
+# /g' $NameWithoutExt".csv"; # insert \n
+#  	sed -i 's/<\/td><td align="right" valign="middle" style="mso-number-format:0\\.00">/,/g' $NameWithoutExt".csv"; # insert ,
+#  	sed -i 's/<\/td><\/tr><\/table><\/body><\/html>//g' $NameWithoutExt".csv"; # all text in line after table
 done
