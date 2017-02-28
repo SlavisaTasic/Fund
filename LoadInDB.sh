07fd2b9493dc7e3b89af6ca3289d5f04ff919cdf
SlavisaTasic <slavisatasic82@gmail.com>
2017-02-28 16:02:27 +0400

    test LoadInDB.sh

--- LoadInDB.sh
+++ LoadInDB.sh
@@ -2,7 +2,7 @@
 
 CurrentDate='date +%d.%m.%Y'
 
-for i in ./Prices/`$CurrentDate`/s/*.csv;
+for i in $Home/Fund/Prices/`$CurrentDate`/*/*.quote;
 do
 	psql \
 	    -h securities.cmcdafitdhnz.us-west-2.rds.amazonaws.com \
@@ -11,30 +11,21 @@ do
 		-w \
 		-d Securities \
 
-		-c "SET datestyle = German;" \
 		-c "\copy PIF(dt, price, net_asset_value) FROM '$i' DELIMITER ',' CSV HEADER"
 done
 
 
-		-c "CREATE TEMP TABLE tmp_PIF ( \
-				issuer text, \
-				full_name text, \
-				symbol text, \
-				dt date, \
-				price NUMERIC(19, 2), \
-				net_asset_value NUMERIC(19, 2) \
-				);" \
-		CREATE TEMP TABLE tmp_x (id int, apple text, banana text); -- but see below
-		COPY tmp_x FROM '/absolute/path/to/file' (FORMAT csv);
-		UPDATE tbl
-		SET    banana = tmp_x.banana
-		FROM   tmp_x
-		WHERE  tbl.id = tmp_x.id;
-		DROP TABLE tmp_x; -- else it is dropped at end of session automatically
+# 		CREATE TEMP TABLE tmp_x (id int, apple text, banana text); -- but see below
+# 		COPY tmp_x FROM '/absolute/path/to/file' (FORMAT csv);
+# 		UPDATE tbl
+# 		SET    banana = tmp_x.banana
+# 		FROM   tmp_x
+# 		WHERE  tbl.id = tmp_x.id;
+# 		DROP TABLE tmp_x; -- else it is dropped at end of session automatically
+# 
+# 		-c "SET datestyle = German;" \
+# 		-c "\copy PIF(dt, price, net_asset_value) FROM '$i' CSV HEADER"
 
-		-c "SET datestyle = German;" \
-		-c "\copy PIF(dt, price, net_asset_value) FROM '$i' CSV HEADER"
-done
 
 #psql_exit_status = $?
 
