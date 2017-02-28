a7fe1257863e2e73f52814928cde40d057565481
SlavisaTasic <slavisatasic82@gmail.com>
2017-02-28 16:20:52 +0400

    test LoadInDB.sh

--- LoadInDB.sh
+++ LoadInDB.sh
@@ -4,13 +4,14 @@ CurrentDate='date +%d.%m.%Y'
 
 for i in $Home/Fund/Prices/`$CurrentDate`/*/*.quote;
 do
-	psql \
-	    -h securities.cmcdafitdhnz.us-west-2.rds.amazonaws.com \
-		-p 5432 \
-		-U master \
-		-w \
-		-d Securities \
-		-c "\copy pif_quotes(symbol, dt, price, NAV) FROM $i CSV HEADER"
+	echo '$i'
+# 	psql \
+# 	    -h securities.cmcdafitdhnz.us-west-2.rds.amazonaws.com \
+# 		-p 5432 \
+# 		-U master \
+# 		-w \
+# 		-d Securities \
+# 		-c "\copy pif_quotes(symbol, dt, price, NAV) FROM $i CSV HEADER"
 		
 done
 
