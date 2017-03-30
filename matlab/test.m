ed13acd1de28da707fac8517d7c4cb819a99aee8
SlavisaTasic <slavisatasic82@gmail.com>
2017-03-31 02:12:43 +0300

    matlab plots

--- matlab/test.m
+++ matlab/test.m
@@ -2,8 +2,8 @@ clear
 clc
 f = figure;
 plot([1:5, [5:-1:1]]);
-print(f, '-r150', '-dpng', 'png/print.png');
+print(f, '-r150', '-dsvg', 'png/print.svg');
 display('print done');
-saveas(f, 'png/saveas.png');
+saveas(f, 'png/saveas.svg');
 display('saveas done');
 close(f);
