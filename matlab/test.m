f2048a2ef696d6ec06dc2b4f89707bba870a2b89
SlavisaTasic <slavisatasic82@gmail.com>
2017-03-31 01:59:58 +0300

    matlab plots

--- matlab/test.m
+++ matlab/test.m
@@ -2,7 +2,7 @@ clear
 clc
 f = figure;
 plot([1:5, [5:-1:1]]);
-print(f, '-r0', '-dpng', 'png/print.png');
+print(f, '-r150', '-dpng', 'png/print.png');
 display('print done');
 saveas(f, 'png/saveas.png');
 display('saveas done');
