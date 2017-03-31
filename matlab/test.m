99367ec3a7bc0a45fa531d0e8bb27aed78f9c421
SlavisaTasic <slavisatasic82@gmail.com>
2017-03-31 03:47:52 +0300

    matlab plots

--- matlab/test.m
+++ matlab/test.m
@@ -7,25 +7,28 @@ ibm = tick2ret(ibm.CLOSE);
 
 fig = figure;
 plot(ibm);
+ytickformat('%,.2f');
 datetick('x', 'mm.yyyy');
 
 
 fig.PaperUnits = 'points';
 fig.PaperPosition = [0 0 600 300];
+
+
 ax = fig.CurrentAxes;
+
+% Appearance
 ax.Box = 'off';
-ax.TickLength = [0.005 0.025];
+
+% Individual Axis Appearance and Scale
+ax.XLim = [min(ibm.dates) max(ibm.dates)];
+
+% Tick Values and Labels
+ax.TickLength = [0.01 0.025];
 ax.TickDir = 'out';
-%ax.Units = 'pixels';
-%ax.OuterPosition 	% red
-%ax.Position			% blue
-%ax.TightInset		% magenta
 
+% Location and Size
 ax.Position = [0.05 0.10 0.90 0.85];
-
-% 432*216
-%		 /[6 3]
-% 72*72
 
 
 tic
