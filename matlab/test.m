951f10770bbc1a1efa2aa22b1a73b4411f1245fd
SlavisaTasic <slavisatasic82@gmail.com>
2017-03-31 04:22:27 +0300

    matlab plots

--- matlab/test.m
+++ matlab/test.m
@@ -9,14 +9,16 @@ ibm_se = std(ibm);
 ibm_se = ibm_se.CLOSE;
 X = ibm.dates;
 Y = fts2mat(ibm);
-
+lower = -1.96*ibm_se+Y;
+upper = 1.96*ibm_se+Y;
 
 fig = figure;
 plot(X, Y);
 ytickformat('%,.2f');
 datetick('x', 'mm.yyyy');
 hold on
-plot(X, [-1.96*ibm_se+Y, 1.96*ibm_se+Y]);
+%plot(X, [-1.96*ibm_se+Y, 1.96*ibm_se+Y]);
+fill([X flip(X)], [upper flip(lower)], 'b');
 hold off
 
 fig.PaperUnits = 'points';
@@ -37,21 +39,6 @@ ax.TickDir = 'out';
 
 % Location and Size
 ax.Position = [0.07 0.10 0.90 0.85];
-
-
-
-clc;
-clear all;
-fig = figure;
-
-a= random('normal',10,4,1,100); % Generate a normally distributed random numbers 
-pd = fitdist(a','normal'); % Creates a ProbDistUnivParam object by fitting the data to a normal distribution. For details look at Documentation
-ci = paramci(pd); % This function calculates the  values of the parameters based on a certain confidence interval. Here the by default the confidence interval is 95 percent
-probplot(a);
-h=probplot(gca,@(a,x,y)normcdf(a,x,y),[ci(1,1),ci(1,2)]); 
-set(h,'color','r','linestyle','-')
-t= probplot(gca,@(a,x,y)normcdf(a,x,y),[ci(2,1),ci(2,2)]);
-set(t,'color','g','linestyle','-.')
 
 
 tic
