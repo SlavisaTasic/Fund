308a657ea5e9e65758e14b892e4a9449e8a196a3
SlavisaTasic <slavisatasic82@gmail.com>
2017-03-31 04:13:30 +0300

    matlab plots

--- matlab/test.m
+++ matlab/test.m
@@ -39,6 +39,21 @@ ax.TickDir = 'out';
 ax.Position = [0.07 0.10 0.90 0.85];
 
 
+
+clc;
+clear all;
+fig = figure;
+
+a= random('normal',10,4,1,100); % Generate a normally distributed random numbers 
+pd = fitdist(a','normal'); % Creates a ProbDistUnivParam object by fitting the data to a normal distribution. For details look at Documentation
+ci = paramci(pd); % This function calculates the  values of the parameters based on a certain confidence interval. Here the by default the confidence interval is 95 percent
+probplot(a);
+h=probplot(gca,@(a,x,y)normcdf(a,x,y),[ci(1,1),ci(1,2)]); 
+set(h,'color','r','linestyle','-')
+t= probplot(gca,@(a,x,y)normcdf(a,x,y),[ci(2,1),ci(2,2)]);
+set(t,'color','g','linestyle','-.')
+
+
 tic
 print(fig, '-r150', '-dsvg', 'png/print.svg');
 toc
